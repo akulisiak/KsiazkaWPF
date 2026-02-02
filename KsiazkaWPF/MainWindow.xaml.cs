b775abbe30588cb25c3260a1d4c8edbf2e12d9b9
Student <Student@TE-KAL-D-S14-03>
2026-02-02 14:58:18 +0100

    Nie działa(do poprawy w domu)

--- KsiazkaWPF/MainWindow.xaml.cs
+++ KsiazkaWPF/MainWindow.xaml.cs
@@ -17,12 +17,58 @@ namespace KsiazkaWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        int i = 1;
         public MainWindow()
         {
             InitializeComponent();
             tylButton.Content = "<<<";
-            przodButton.Content = ">>>";   
+            przodButton.Content = ">>>";
 
+           
+        }
+
+        private void przodButton_Click(object sender, RoutedEventArgs e)
+        {
+            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\dane.txt");
+            string[] tablicaStron = daneZPliku.Split(';');
+
+            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+
+            for (int i = 1; i < tablicaStron.Length; i++)
+            {
+                ksiazka.DodajStrone(tablicaStron[i]);
+            }
+
+            tytulLabel.Content = "Tytuł książki: " + ksiazka.strony[0].trescStrony;
+
+            lewaTextBlock.Text = ksiazka.strony[i].trescStrony;
+            prawaTextBlock.Text = ksiazka.strony[i + 1].trescStrony;
+            i += 2;
+        }
+
+        private void tylButton_Click(object sender, RoutedEventArgs e)
+        {
+            i -= 2;
+            string daneZPliku = File.ReadAllText(@"C:\Users\Student\Desktop\dane.txt");
+            string[] tablicaStron = daneZPliku.Split(';');
+
+            Ksiazka ksiazka = new Ksiazka(tablicaStron[0]);
+
+            for (int i = 1; i < tablicaStron.Length; i++)
+            {
+                ksiazka.DodajStrone(tablicaStron[i]);
+            }
+
+            tytulLabel.Content = "Tytuł książki: " + ksiazka.strony[0].trescStrony;
+
+            lewaTextBlock.Text = ksiazka.strony[i].trescStrony;
+            prawaTextBlock.Text = ksiazka.strony[i + 1].trescStrony;
+            
+        }
+
+        private void HandleChange(object sender, TextChangedEventArgs e)
+        {
+            
         }
     }
 }
